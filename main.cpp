6584d6fc7974bceb5c8c19c46708c44c68b4665c
Konstantin S <turnever@gmail.com>
2018-01-16 23:02:39 +1000

    add new test & time-testing

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main() {
+    clock_t time;
+    time = clock();
 
     Graph g;
-    g.ReadGraphFromXML("test.xml");
+    //g.ReadGraphFromXML("RU-MAG-1.osm");
+    //g.ReadGraphFromXML("test1.xml");
+    g.ReadGraphFromXML("test2.xml");
 
+    time = clock() - time;
+    std::cout << time/CLOCKS_PER_SEC << endl;
     return 0;
 }
