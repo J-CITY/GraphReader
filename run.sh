e68b9b1c55b028b77f7de55e766fcd6df7526da8
Konstantin S <turnever@gmail.com>
2018-01-19 15:12:18 +1000

    modified main.w and run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-cd report;
-rm report.pdf;
-cd core/ && nuweb main.w && cd ..;
-mv core/main.cpp core/Graph.* ../;
-pdflatex report.tex;
+cd ./report;
+rm ./report.pdf;
+cd ./core/ && nuweb ./main.w && cd ..;
+mv ./core/main.cpp ./core/Graph.* ../;
+pdflatex ./report.tex;
+cd ./core/ && nuweb ./main.w && cd ..;
+mv ./core/main.cpp ./core/Graph.* ../;
 cd ..;
 cmake . && make;
